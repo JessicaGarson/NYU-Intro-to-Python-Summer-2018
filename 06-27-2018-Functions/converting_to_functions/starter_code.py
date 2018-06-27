9e0d8c4254b294d1c03e3fb30f7c217e834dc021
JessicaGarson <jessica.garson@gmail.com>
2018-06-27 12:58:38 -0400

    fixing starter

--- 06-27-2018-Functions/converting_to_functions/starter_code.py
+++ 06-27-2018-Functions/converting_to_functions/starter_code.py
@@ -1,6 +1,6 @@
 twitter_max = input("What is the maximum number of characters permitted in twitter posts?\n")
 
-tweet = input("What would you like to tweet? ")
+tweet = input("What would you like to tweet?\n")
 
 len_tweet = len(tweet)
 print(len_tweet)
@@ -10,6 +10,5 @@ len_remain = len_allowed - len_tweet
 
 if len_remain >= 0:
     print('That tweet is {} characters and you have {} remaining characters' .format(len_tweet, len_remain))
-
 else:
     print('That tweet is {} characters and you have to trim by {} characters').format(len_tweet, - len_remain)
