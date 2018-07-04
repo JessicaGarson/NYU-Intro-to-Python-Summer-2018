90071c32627c7032221e5f5a8aab38a83f6098d7
JessicaGarson <jessica.garson@gmail.com>
2018-07-04 14:21:06 -0400

    adding thing

--- 06-27-2018-Functions/Converting_to_functions/homework_with_functions.py
+++ 06-27-2018-Functions/Converting_to_functions/homework_with_functions.py
@@ -21,20 +21,20 @@ def remain(len_allowed, len_tweet):
     return len_allowed - len_tweet
 
 
-def logic(len_allowed, len_remain):
+def logic(tweet_length, len_allowed, len_remain):
     if len_remain <= len_allowed:
-        print('That tweet is {} characters and you have {} remaining characters'.format(len_allowed, len_remain))
+        print('That tweet is {} characters and you have {} remaining characters'.format(tweet_length, len_remain))
     else:
         print('That tweet is {} characters and you have to trim by {} characters'.format(len_allowed, len_allowed - len_remain))
 
 
 def main():
     tweet_length = max_tweet()
-    int_tweet_max = convert_int(twitter_max=tweet_length)
+    tweet_length_int = convert_int(tweet_length)
     what_to_tweet = tweet()
     tweet_length = tw_length(tweet=what_to_tweet)
-    len_remain = remain(len_allowed=int_tweet_max, len_tweet=tweet_length)
-    logic(len_allowed=int_tweet_max, len_remain=len_remain)
+    len_remain = remain(len_allowed=tweet_length_int, len_tweet=tweet_length)
+    logic(tweet_length=tweet_length, len_allowed=tweet_length_int, len_remain=len_remain)
 
 
 if __name__ == '__main__':
