4f774ffa65315a976ec730f6478d952e8a6d143a
JessicaGarson <jessica.garson@gmail.com>
2018-08-01 14:40:23 -0400

    adding solutions

---
+++ 08-01-2018-Data-Science-Intro/code_and_files/solution.py
@@ -0,0 +1,17 @@
+abbv = 'AK, AL, AR, AZ, CA, CO, CT, DC, DE, FL, GA, HI, IA, ID, IL, IN, KS, KY, LA, MA, MD, ME, MI, MN, MO, MS, MT, NC, ND, NE, NH, NJ, NM, NV, NY, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VA, VT, WA, WI, WV, WY'
+
+state_abbreviations = abbv.split(',')
+
+state_names = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, District of Columbia, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming'
+
+states = state_names.split(',')
+
+
+def createSelectStateHTML(abbrev, state):
+    print('<select>')
+    for state_code, state_name in zip(abbrev, state):
+        print('\t<option value="{}">{}</option>'.format(state_code, state_name))
+    print('</select>')
+
+
+createSelectStateHTML(abbrev=state_abbreviations, state=states)
